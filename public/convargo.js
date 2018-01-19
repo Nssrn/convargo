cec9ddb26c2ee70b99d065368abbfd8c250e0228
Nissrine <anissrine95@hotmail.fr>
2018-01-19 18:43:30 +0100

    New price with Discount perVolume

--- public/convargo.js
+++ public/convargo.js
@@ -13,17 +13,32 @@ deliveries.forEach(function (delivery) {
 function getPriceByDelivery(delivery) {
     var pricePerKm = 0;
     var pricePerVolume = 0;
+    var truck = null;
+
+
     truckers.forEach(function (trucker) {
         if (trucker.id === delivery.truckerId) {
             pricePerKm = trucker.pricePerKm;
             pricePerVolume = trucker.pricePerVolume;
+            truck = trucker;
         }
     });
     var distance = delivery.distance;
     var volume = delivery.volume;
-    return getPricePerDistanceVolume(distance, pricePerKm, volume, pricePerVolume);
+    return getPricePerDistanceVolume(distance, pricePerKm, volume, pricePerVolume, truck);
 }
 
-function getPricePerDistanceVolume(distance, pricePerKm, volume, pricePerVolume) {
-    return distance * pricePerKm + volume * pricePerVolume;
+
+function getPricePerDistanceVolume(distance, pricePerKm, volume, pricePerVolume,truck) {
+	console.log(truck.pricePerVolume);
+	var price = distance * pricePerKm + volume * pricePerVolume;
+	
+
+	if (truck.pricePerVolume > 5) {price = price - (price * 10/100);}
+	if (truck.pricePerVolume > 10) {price = price - (price * 30/100);}
+	if (truck.pricePerVolume > 25) {price = price - (price * 50/100);}
+
+
+    return price;
 }
+
